9d629764a3adfdc6eabf6b96777108825b96fc02
Christopher Serr <christopher.serr@gmail.com>
2022-11-28 18:17:09 +0100

    Support `mGBA` (#8)
    This adds support for `mGBA` as the Gameboy Advance emulator.

--- src/gba.rs
+++ src/gba.rs
@@ -4,13 +4,13 @@ use crate::{runtime, Address, Process};
 
 pub struct Emulator {
     process: Process,
-    ewram: u32,
-    iwram: u32,
+    ewram: u64,
+    iwram: u64,
 }
 
 impl Emulator {
     pub fn attach() -> Option<Self> {
-        look_for_vba()
+        look_for_mgba().or_else(look_for_vba)
     }
 
     pub fn is_open(&self) -> bool {
@@ -24,9 +24,30 @@ impl Emulator {
             3 => self.iwram,
             _ => return Err(runtime::Error),
         };
-        let addr = ram_addr + (address & 0xFF_FF_FF);
-        self.process.read(Address(addr as u64))
+        let addr = ram_addr + (address & 0xFF_FF_FF) as u64;
+        self.process.read(Address(addr))
     }
+}
+
+fn look_for_mgba() -> Option<Emulator> {
+    let process = Process::attach("mGBA.exe")?;
+
+    let [ewram, iwram]: [u64; 2] = process
+        .read_pointer_path64(
+            process.get_module_address("mGBA.exe").ok()?.0,
+            &[0x01D01868, 0x38, 0x10, 0x8, 0x28],
+        )
+        .ok()?;
+
+    if ewram == 0 || iwram == 0 {
+        return None;
+    }
+
+    Some(Emulator {
+        process,
+        ewram,
+        iwram,
+    })
 }
 
 fn look_for_vba() -> Option<Emulator> {
@@ -37,7 +58,7 @@ fn look_for_vba() -> Option<Emulator> {
     }
     Some(Emulator {
         process,
-        ewram,
-        iwram,
+        ewram: ewram as u64,
+        iwram: iwram as u64,
     })
 }
