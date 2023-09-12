6a634360108e6c58dc4254bb93ae3d359b8b2bd3
Jujstme <just.tribe@gmail.com>
2023-09-12 22:21:42 +0200

    Refactored the code to be more rust-y

--- src/emulator/gcn/dolphin.rs
+++ src/emulator/gcn/dolphin.rs
@@ -5,29 +5,27 @@ pub struct State;
 
 impl State {
     pub fn find_ram(&mut self, game: &Process, endian: &mut Endian) -> Option<Address> {
+        *endian = Endian::Big;
+
         // Main logic: finding the address for the GCN main memory by looking for
-        // memory ranges with the READ and WRITE flags and a size of 0x2000000
-        for entry in game.memory_ranges() {
-            if entry.size().is_ok_and(|size| size == 0x2000000)
-                && entry.flags().is_ok_and(|flag| {
-                    flag.contains(MemoryRangeFlags::WRITE | MemoryRangeFlags::READ)
-                })
-            {
-                if let Ok(addr) = entry.address() {
-                    // In order to verify this is the correct memory range, we take advantage
-                    // of a small 'hack', by checking if the offset 0x1C contains a "magic number"
-                    // fixed for all Gamecube games.
-                    if game
-                        .read::<u32>(addr + 0x1C)
-                        .is_ok_and(|magic| magic.from_endian(Endian::Big) == 0xC2339F3D)
-                    {
-                        *endian = Endian::Big;
-                        return Some(addr);
-                    }
-                }
-            }
-        }
-        None
+        // memory ranges with the READ and WRITE flags and a size of 0x2000000.
+        // In order to verify we found the correct memory range, we take advantage
+        // of a small 'hack', by checking if the offset 0x1C contains a "magic number"
+        // fixed for all Gamecube games.
+
+        game.memory_ranges()
+            .find(|range| {
+                range
+                    .flags()
+                    .is_ok_and(|r| r.contains(MemoryRangeFlags::WRITE | MemoryRangeFlags::READ))
+                    && range.size().is_ok_and(|size| size == 0x2000000)
+                    && range.address().is_ok_and(|addr| {
+                        game.read::<u32>(addr + 0x1C)
+                            .is_ok_and(|magic| magic.from_endian(Endian::Big) == 0xC2339F3D)
+                    })
+            })?
+            .address()
+            .ok()
     }
 
     pub fn keep_alive(&self, game: &Process, ram_base: &Option<Address>) -> bool {
