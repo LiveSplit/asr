d7eb548a2cc29585e4ed34845ccff0f9a932be6c
Jujstme <just.tribe@gmail.com>
2023-10-08 17:05:57 +0200

    Finished

--- src/game_engine/unity/scene.rs
+++ src/game_engine/unity/scene.rs
@@ -2,6 +2,8 @@
 
 // References:
 // https://gist.githubusercontent.com/just-ero/92457b51baf85bd1e5b8c87de8c9835e/raw/8aa3e6b8da01fd03ff2ff0c03cbd018e522ef988/UnityScene.hpp
+// (some offsets seem to be wrong anyway, but it's a very good starting point)
+//
 // Offsets and logic for Transforms and GameObjects taken from https://github.com/Micrologist/UnityInstanceDumper
 
 use core::{array, iter, mem::MaybeUninit};
@@ -36,7 +38,9 @@ impl SceneManager {
         let is_64_bit = pe::MachineType::read(process, unity_player.0)? == pe::MachineType::X86_64;
         let is_il2cpp = process.get_module_address("GameAssembly.dll").is_ok();
 
-        let address = if is_64_bit {
+        // There are multiple signatures that can be used, depending on the version of Unity
+        // used in the target game.
+        let base_address: Address = if is_64_bit {
             let addr = SIG_64_BIT.scan_process_range(process, unity_player)? + 7;
             addr + 0x4 + process.read::<i32>(addr).ok()?
         } else if let Some(addr) = SIG_32_1.scan_process_range(process, unity_player) {
@@ -51,12 +55,23 @@ impl SceneManager {
 
         let offsets = Offsets::new(is_64_bit);
 
-        Some(Self {
-            is_64_bit,
-            is_il2cpp,
-            address,
-            offsets,
-        })
+        // Dereferencing one level because this pointer never changes as long as the game is open.
+        // It might not seem a lot, but it helps make things a bit faster when querying for scene stuff.
+        let address: Address = match is_64_bit {
+            true => process.read::<Address64>(base_address).ok()?.into(),
+            false => process.read::<Address32>(base_address).ok()?.into(),
+        };
+
+        if address.is_null() {
+            None
+        } else {
+            Some(Self {
+                is_64_bit,
+                is_il2cpp,
+                address,
+                offsets,
+            })
+        }
     }
 
     /// Attaches to the scene manager in the given process.
@@ -74,18 +89,28 @@ impl SceneManager {
         })
     }
 
-    fn get_current_scene_address(&self, process: &Process) -> Result<Address, Error> {
-        let addr = self.read_pointer(process, self.address)?;
-        self.read_pointer(process, addr + self.offsets.active_scene)
+    fn pointer_size(&self) -> u8 {
+        match self.is_64_bit {
+            true => 8,
+            false => 4,
+        }
+    }
+
+    /// Tries to retrieve the current active scene.
+    fn get_current_scene(&self, process: &Process) -> Result<Scene, Error> {
+        Ok(Scene {
+            address: self.read_pointer(process, self.address + self.offsets.active_scene)?,
+        })
     }
 
     /// `DontDestroyOnLoad` is a special Unity scene containing game objects
     /// that must be preserved when switching between different scenes (eg. a
     /// `scene1` starting some background music that continues when `scene2`
     /// loads).
-    fn get_dont_destroy_on_load_scene_address(&self, process: &Process) -> Result<Address, Error> {
-        let addr = self.read_pointer(process, self.address)?;
-        Ok(addr + self.offsets.dont_destroy_on_load_scene)
+    fn get_dont_destroy_on_load_scene(&self) -> Scene {
+        Scene {
+            address: self.address + self.offsets.dont_destroy_on_load_scene,
+        }
     }
 
     /// Returns the current scene index.
@@ -93,24 +118,20 @@ impl SceneManager {
     /// The value returned is a [`i32`] because some games will show `-1` as their
     /// current scene until fully initialized.
     pub fn get_current_scene_index(&self, process: &Process) -> Result<i32, Error> {
-        process.read(self.get_current_scene_address(process)? + self.offsets.build_index)
+        self.get_current_scene(process)?.index(process, self)
     }
 
     /// Returns the full path to the current scene. Use
-    /// [`get_current_scene_path`](Self::get_current_scene_path) afterwards to
+    /// [`get_scene_name`](Self::get_scene_name) afterwards to
     /// get the scene name.
     pub fn get_current_scene_path<const N: usize>(
         &self,
         process: &Process,
     ) -> Result<ArrayCString<N>, Error> {
-        let addr = self.read_pointer(
-            process,
-            self.get_current_scene_address(process)? + self.offsets.asset_path,
-        )?;
-        process.read(addr)
+        self.get_current_scene(process)?.path(process, self)
     }
 
-    /// Returns the number of total scenes in the attached game.
+    /// Returns the number of currently loaded scenes in the attached game.
     pub fn get_scene_count(&self, process: &Process) -> Result<u32, Error> {
         process.read(self.address + self.offsets.scene_count)
     }
@@ -120,21 +141,28 @@ impl SceneManager {
         &'a self,
         process: &'a Process,
     ) -> impl DoubleEndedIterator<Item = Scene> + 'a {
-        let fptr = self.read_pointer(process, self.address).unwrap_or_default();
-        let addr = self
-            .read_pointer(process, fptr + self.offsets.loaded_scenes)
-            .unwrap_or_default();
+        let (num_scenes, addr): (usize, Address) = if self.is_64_bit {
+            let [first, _, third] = process
+                .read::<[u64; 3]>(self.address + self.offsets.scene_count)
+                .unwrap_or_default();
+            (first as usize, Address::new(third))
+        } else {
+            let [first, _, third] = process
+                .read::<[u32; 3]>(self.address + self.offsets.scene_count)
+                .unwrap_or_default();
+            (first as usize, Address::new(third as _))
+        };
 
-        let pointer_size = if self.is_64_bit { 8 } else { 4 };
-
-        (0..16)
-            .map(move |index| Scene {
-                address: {
-                    self.read_pointer(process, addr + (index as u64).wrapping_mul(pointer_size))
-                        .unwrap_or_default()
-                },
-            })
-            .filter(move |p| !fptr.is_null() && p.is_valid(process))
+        (0..num_scenes).filter_map(move |index| {
+            if let Ok(address) = self.read_pointer(
+                process,
+                addr + (index as u64).wrapping_mul(self.pointer_size() as _),
+            ) {
+                Some(Scene { address })
+            } else {
+                None
+            }
+        })
     }
 
     /// Iterates over all root [`Transform`]s declared for the
@@ -148,10 +176,12 @@ impl SceneManager {
     fn root_game_objects<'a>(
         &'a self,
         process: &'a Process,
-        scene_address: Address,
+        scene: &Scene,
     ) -> Result<impl Iterator<Item = Transform> + 'a, Error> {
-        let list_first =
-            self.read_pointer(process, scene_address + self.offsets.root_storage_container)?;
+        let list_first = self.read_pointer(
+            process,
+            scene.address + self.offsets.root_storage_container,
+        )?;
 
         let mut current_list = list_first;
         let mut iter_break = false;
@@ -185,7 +215,7 @@ impl SceneManager {
     /// Tries to find the specified root [`Transform`] from the currently
     /// active Unity scene.
     pub fn get_root_game_object(&self, process: &Process, name: &str) -> Result<Transform, Error> {
-        self.root_game_objects(process, self.get_current_scene_address(process)?)?
+        self.root_game_objects(process, &self.get_current_scene(process)?)?
             .find(|obj| {
                 obj.get_name::<128>(process, self)
                     .is_ok_and(|obj_name| obj_name.matches(name))
@@ -200,15 +230,12 @@ impl SceneManager {
         process: &Process,
         name: &str,
     ) -> Result<Transform, Error> {
-        self.root_game_objects(
-            process,
-            self.get_dont_destroy_on_load_scene_address(process)?,
-        )?
-        .find(|obj| {
-            obj.get_name::<128>(process, self)
-                .is_ok_and(|obj_name| obj_name.matches(name))
-        })
-        .ok_or(Error {})
+        self.root_game_objects(process, &self.get_dont_destroy_on_load_scene())?
+            .find(|obj| {
+                obj.get_name::<128>(process, self)
+                    .is_ok_and(|obj_name| obj_name.matches(name))
+            })
+            .ok_or(Error {})
     }
 }
 
@@ -309,7 +336,7 @@ impl Transform {
                         true => {
                             let Ok(name_ptr) = scene_manager.read_pointer(
                                 process,
-                                vtable + 2 * if scene_manager.is_64_bit { 8 } else { 4 },
+                                vtable + 2 * scene_manager.pointer_size(),
                             ) else {
                                 return false;
                             };
@@ -346,22 +373,22 @@ impl Transform {
         scene_manager: &'a SceneManager,
     ) -> Result<impl Iterator<Item = Self> + 'a, Error> {
         let (child_count, child_pointer): (usize, Address) = if scene_manager.is_64_bit {
-            let array = process
-                .read::<[Address64; 3]>(self.address + scene_manager.offsets.children_pointer)?;
-            (array[2].value() as usize, array[0].into())
+            let [first, _, third] = process
+                .read::<[u64; 3]>(self.address + scene_manager.offsets.children_pointer)?;
+            (third as usize, Address::new(first))
         } else {
-            let array = process
-                .read::<[Address32; 3]>(self.address + scene_manager.offsets.children_pointer)?;
-            (array[2].value() as usize, array[0].into())
+            let [first, _, third] = process
+                .read::<[u32; 3]>(self.address + scene_manager.offsets.children_pointer)?;
+            (third as usize, Address::new(first as _))
         };
 
-        if child_count == 0 {
-            return Err(Error {});
-        }
-
-        // Define an empty array and fill it later with the addresses of all child classes found for the current GameObject.
+        // Define an empty array and fill it later with the addresses of all child classes found for the current Transform.
         // Reading the whole array of pointers is (slightly) faster than reading each address in a loop
         const ARRAY_SIZE: usize = 128;
+
+        if child_count == 0 || child_count > ARRAY_SIZE {
+            return Err(Error {});
+        }
 
         let children: [Address; ARRAY_SIZE] = if scene_manager.is_64_bit {
             let mut buf = [MaybeUninit::<Address64>::uninit(); ARRAY_SIZE];
@@ -377,14 +404,12 @@ impl Transform {
             array::from_fn(|_| iter.next().copied().map(Into::into).unwrap_or_default())
         };
 
-        Ok((0..child_count).map(move |f| {
-            Self {
-                address: children[f]
-            }
+        Ok((0..child_count).map(move |f| Self {
+            address: children[f],
         }))
     }
 
-    /// Tries to find a child `GameObject` from the current one.
+    /// Tries to find a child `Transform` from the current one.
     pub fn get_child(
         &self,
         process: &Process,
@@ -402,7 +427,6 @@ impl Transform {
 
 struct Offsets {
     scene_count: u8,
-    loaded_scenes: u8,
     active_scene: u8,
     dont_destroy_on_load_scene: u8,
     asset_path: u8,
@@ -410,10 +434,8 @@ struct Offsets {
     root_storage_container: u8,
     game_object: u8,
     game_object_name: u8,
-    //number_of_object_components: u8,
     klass: u8,
     klass_name: u8,
-    //children_count: u8,
     children_pointer: u8,
 }
 
@@ -422,7 +444,6 @@ impl Offsets {
         match is_64_bit {
             true => &Self {
                 scene_count: 0x18,
-                loaded_scenes: 0x28,
                 active_scene: 0x48,
                 dont_destroy_on_load_scene: 0x70,
                 asset_path: 0x10,
@@ -430,15 +451,12 @@ impl Offsets {
                 root_storage_container: 0xB0,
                 game_object: 0x30,
                 game_object_name: 0x60,
-                //number_of_object_components: 0x40,
                 klass: 0x28,
                 klass_name: 0x48,
-                //children_count: 0x80,
                 children_pointer: 0x70,
             },
             false => &Self {
-                scene_count: 0xC,
-                loaded_scenes: 0x18,
+                scene_count: 0x10,
                 active_scene: 0x28,
                 dont_destroy_on_load_scene: 0x40,
                 asset_path: 0xC,
@@ -446,10 +464,8 @@ impl Offsets {
                 root_storage_container: 0x88,
                 game_object: 0x1C,
                 game_object_name: 0x3C,
-                //number_of_object_components: 0x24,
                 klass: 0x18,
                 klass_name: 0x2C,
-                //children_count: 0x58,
                 children_pointer: 0x50,
             },
         }
@@ -475,7 +491,7 @@ impl Scene {
 
     /// Returns the build index of the scene. This index is unique to each
     /// scene in the game.
-    pub fn index(&self, process: &Process, scene_manager: &SceneManager) -> Result<u32, Error> {
+    pub fn index(&self, process: &Process, scene_manager: &SceneManager) -> Result<i32, Error> {
         process.read(self.address + scene_manager.offsets.build_index)
     }
 
