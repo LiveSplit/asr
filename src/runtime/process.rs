395628e22f87a4ff38f6d34447884202b83c3635
AlexKnauth <alexander@knauth.org>
2023-10-31 02:47:47 -0400

    Add process.get_module_path

--- src/runtime/process.rs
+++ src/runtime/process.rs
@@ -194,6 +194,42 @@ impl Process {
         }
     }
 
+    /// Gets the path of a module in the file system. The path is a path that is
+    /// accessible through the WASI file system, so a Windows path of
+    /// `C:\foo\bar.dll` would be returned as `/mnt/c/foo/bar.dll`.
+    #[cfg(feature = "alloc")]
+    #[inline]
+    pub fn get_module_path(&self, name: &str) -> Result<alloc::string::String, Error> {
+        // SAFETY: Calling `process_get_module_path` with a null pointer and 0
+        // length will return the required length. We then allocate a buffer
+        // with the required length and call it again with the buffer. We then
+        // convert the buffer into a string, which is guaranteed to be valid
+        // UTF-8.
+        unsafe {
+            let mut len = 0;
+            sys::process_get_module_path(
+                self.0,
+                name.as_ptr(),
+                name.len(),
+                core::ptr::null_mut(),
+                &mut len,
+            );
+            let mut buf = alloc::vec::Vec::with_capacity(len);
+            let success = sys::process_get_module_path(
+                self.0,
+                name.as_ptr(),
+                name.len(),
+                buf.as_mut_ptr(),
+                &mut len,
+            );
+            if !success {
+                return Err(Error {});
+            }
+            buf.set_len(len);
+            Ok(alloc::string::String::from_utf8_unchecked(buf))
+        }
+    }
+
     /// Gets the address and size of a module in the process.
     #[inline]
     pub fn get_module_range(&self, name: &str) -> Result<(Address, u64), Error> {
