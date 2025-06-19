e50491710b7cd3bb197fa00f4faa2e2a37468e1a
Jujstme <just.tribe@gmail.com>
2025-06-19 23:57:21 +0200

    Fixed PCSX_redux signscans again

--- src/emulator/ps1/pcsx_redux.rs
+++ src/emulator/ps1/pcsx_redux.rs
@@ -3,7 +3,7 @@ use crate::{file_format::pe, signature::Signature, Address, Address64, PointerSi
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct State {
     addr_base: Address,
-    offsets: Option<[u64; 3]>,
+    offsets: [u64; 3],
 }
 
 impl State {
@@ -17,53 +17,42 @@ impl State {
             return None;
         }
 
-        const SIG_BASE: Signature<14> = Signature::new("48 8B 0D ?? ?? ?? ?? ?? ?? ?? FF FF FF 00");
-        const SIG_OFFSET: Signature<10> = Signature::new("4C 8B 99 ?? ?? ?? ?? 4D 8B 7B");
+        const SIG_BASE: Signature<19> =
+            Signature::new("48 8B 05 ?? ?? ?? ?? 48 8B 80 ?? ?? ?? ?? 48 8B 50 ?? E8");
 
-        self.addr_base = SIG_BASE
-            .scan_process_range(game, main_module_range)
-            .map(|val| val + 3)
-            .and_then(|addr| Some(addr + 0x4 + game.read::<i32>(addr).ok()?))?;
+        let addr = SIG_BASE.scan_process_range(game, main_module_range)? + 3;
 
-        let p_addr = SIG_OFFSET.scan_process_range(game, main_module_range)?;
-        self.offsets = Some([
+        self.addr_base = addr + 0x4 + game.read::<i32>(addr).ok()?;
+
+        self.offsets = [
             0,
-            game.read::<i32>(p_addr + 3).ok()? as u64,
-            game.read::<u8>(p_addr + 10).ok()?.into(),
-        ]);
+            game.read::<i32>(addr + 7).ok()? as u64,
+            game.read::<u8>(addr + 14).ok()?.into(),
+        ];
 
-        game.read_pointer_path::<Address64>(
-            self.addr_base,
-            PointerSize::Bit64,
-            &self.offsets.unwrap(),
-        )
-        .map(|val| val.into())
-        .ok()
+        game.read_pointer_path::<Address64>(self.addr_base, PointerSize::Bit64, &self.offsets)
+            .map(|val| val.into())
+            .ok()
     }
 
     pub fn keep_alive(&self, game: &Process, ram_base: &mut Option<Address>) -> bool {
-        match self.offsets {
-            None => false,
-            Some(offsets) => {
-                match game.read_pointer_path::<Address64>(
-                    self.addr_base,
-                    PointerSize::Bit64,
-                    &offsets,
-                ) {
-                    Ok(result) => {
-                        *ram_base = Some(result.into());
-                        true
-                    }
-                    Err(_) => false,
-                }
+        match game
+            .read_pointer_path::<Address64>(self.addr_base, PointerSize::Bit64, &self.offsets)
+            .ok()
+            .filter(|addr| !addr.is_null())
+        {
+            Some(result) => {
+                *ram_base = Some(result.into());
+                true
             }
+            None => false,
         }
     }
 
     pub const fn new() -> Self {
         Self {
             addr_base: Address::NULL,
-            offsets: None,
+            offsets: [0; 3],
         }
     }
 }
